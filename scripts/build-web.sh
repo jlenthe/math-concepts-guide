8111a368e4af123e4818872393336bf0a066c364
Jason Lenthe <12025654+jlenthe@users.noreply.github.com>
2025-05-25 21:21:05 -0400

    Add a github action for canary build

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -12,9 +12,7 @@ cp "$SRC_DIR"/*.tex "$OUT_DIR/"
 cp -r "$SRC_DIR"/images "$OUT_DIR/"
 
 # Run make4ht inside the output directory
-(
-    cd "$OUT_DIR"
-    make4ht "$TEX_FILE" "mathjax,mathml,html5"
-    rm -f ./*.tex  # Clean up copied .tex files
-)
+cd "$OUT_DIR"
+make4ht "$TEX_FILE" "mathjax,mathml,html5"
+rm -f ./*.tex  # Clean up copied .tex files
 
