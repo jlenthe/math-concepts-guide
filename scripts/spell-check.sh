53821a40dc4e1635a37eb7e8684bbb5e6c463f41
Jason Lenthe <12025654+jlenthe@users.noreply.github.com>
2025-06-19 21:26:38 -0400

    Add check for aspell to spell check script

--- scripts/spell-check.sh
+++ scripts/spell-check.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
+if ! command -v aspell >/dev/null 2>&1; then
+  echo "Error: aspell is not installed." >&2
+  exit 1
+fi
 
 for file in src/*.tex; do
   aspell --mode=tex --personal=./word-list.txt --dont-backup check "$file"
